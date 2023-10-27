4f0cfb3bdc3a6de5171d95245f6a1ec5a4d76594
ychenxiang <106875116+ychenxiang@users.noreply.github.com>
2023-10-27 23:02:22 +0800

    購票辨識與檢視所有活動

--- user/models.py
+++ user/models.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify, request, render_template,session,redirect,Response
+from flask import Flask, jsonify, request, render_template, session, redirect, Response, url_for
 import pymongo
 import json
 from bson import json_util
 from passlib.hash import pbkdf2_sha256
 from bson.objectid import ObjectId
-
+import re
 
 myclient = pymongo.MongoClient("mongodb+srv://team17:TqZI3KaT56q6xwYZ@team17.ufycbtt.mongodb.net/")
 mydb = myclient.test
@@ -36,7 +36,7 @@ class User:
 
         user_json = json_util.dumps(user)
 
-        if not mydb.users.find_one({ "email": user['email']}):
+        if not mydb.users.find_one({ "email": user['email'] }):
             mydb.users.insert_one(user)
             return self.start_session(user)
 
@@ -45,18 +45,6 @@ class User:
 
         else:
             return jsonify({ "error": "something's wrong..."}), 400
-
-        '''
-        #checking for existing email
-        if mydb.users.find_one({ "email": user['email']}):
-           return jsonify({ "error": "email address already exist"}), 400
-        
-        if mydb.users.insert_one(user):
-
-            return self.start_session(user)
-        
-        return jsonify({"error": "sign up error"}), 400
-        '''
 
     def signout(self):
         session.clear()
@@ -83,7 +71,6 @@ class User:
         user_json = session.get('user')  # Get user JSON from session
         user_data = json.loads(user_json)  # Parse JSON to dictionary
         user_email = user_data['email']
-        
         
         if not user_email:
             return jsonify({"error": "Email not found in session"}), 400
@@ -126,4 +113,131 @@ class User:
             return json_util.dumps(updated_user)
         
         return jsonify({"error": "Update failed"}), 400
+    
+    def get_all_member(self):
+        try:
+            members = mydb.users.find({},{"name":1, "email":1})
+            #print("passed models.py, reaching for db")
+            return render_template('admin.html', members = members)
+        except Exception as e:
+            print("Error getting all member")
+            return json_util.dumps({'error' : str(e)})
 
+    def delete_member(self, email):
+        try:
+            mydb.users.delete_one({"email": email})
+            return redirect('/admin')
+        except Exception as e:
+            print("Error deleting member:", str(e))
+            return {'error': str(e)}
+
+    #個別會員的資料
+    def get_member(self, email):
+        try:
+            members = mydb.users.find({},{"name":1, "email":1})
+    
+            try:
+                member_info = mydb.users.find_one({"email":email}, {"_id":0, "password":0})
+                #test_member_json = json_util.dumps(test_member)
+                print(member_info)
+                return render_template('admin.html', members = members, member_info = member_info)
+
+            except Exception as e:
+                print("Error (inside) get member info: ", str(e))
+                return json_util.dumps({'error' : str(e)})
+
+        except Exception as e:
+            print("Error (outside) get all member: ", str(e))
+            return json_util.dumps({'error' : str(e)})
+
+    ### testing    
+    def test_get_all_member(self):
+        def is_admin(self):
+            try:
+                members = mydb.users.find({}, {"name": 1, "email": 1})
+                return render_template('test.html', members=members)
+                user_json = session.get('user')
+                if user_json:
+                    user_data = json.laods(user_json)
+                    if user_data['email'] == 'ncumis.team17@gmail.com':
+                        return render_template('test.html')
+                    else:
+                        return render_template('test.html')
+                else:
+                    print("user_json is null")
+                    return render_template("/test_error")
+
+            except Exception as e:
+                print("error")
+                return json_util.dumps({'error': str(e)})
+    ### end testing
+
+    def search(self):
+        if request.method == 'POST':
+            keyword = request.form['keyword']
+            # 使用正则表达式进行模糊搜索，查询多个字段
+            regex = re.compile(f'.*{keyword}.*', re.IGNORECASE)
+
+            # 使用 $or 运算符来构建查询条件，以同时搜索 "name" 和 "email" 字段
+            query = {
+                "$or": [
+                    {"title": regex},  # 搜索 "name" 字段
+                    {"description": regex}  # 搜索 "email" 字段
+                ]
+            }
+
+            results = list(mydb.events.find(query))  # 将结果转换为列表
+            return render_template('search.html', results=results)
+
+    def event_details(self, event_id):
+        # 使用 event_id 检索事件的详细信息，然后将详细信息传递给模板
+        event = mydb.events.find_one({"_id": ObjectId(event_id)})  # 假设您的事件具有唯一的 _id
+        return render_template('event.html', event=event)
+
+
+    def all_event():
+        all_events = list(mydb.events.find({}, {'_id': 1, 'title': 1}))
+        chinese_events = list(mydb.events.find({'category': '1'}, {'_id': 1, 'title': 1}))
+        korean_events = list(mydb.events.find({'category': '2'}, {'_id': 1, 'title': 1}))
+        japanese_events = list(mydb.events.find({'category': '3'}, {'_id': 1, 'title': 1}))
+        western_events = list(mydb.events.find({'category': '4'}, {'_id': 1, 'title': 1}))
+
+        return render_template('all_event.html',
+                               all_events=all_events,
+                               chinese_events=chinese_events,
+                               korean_events=korean_events,
+                               japanese_events=japanese_events,
+                               western_events=western_events)
+
+
+class Event:
+    def delete_event(self):
+        return 0
+
+    def add_event(self):
+
+        if request.form.get('password') != request.form.get('password_confirm'):
+            return jsonify({ "error": "Confirm Password must match"}), 401
+
+        event = {
+            "title": request.form.get('title'),
+            "time": request.form.get('time'),  #date+time?
+            "ticket_time": request.form.get('ticket_time'),
+            "ticket_price": request.form.get('ticket_price'),
+            "ticket_amount": request.form.get('ticket_amount'),
+            "description": request.form.get('description'),
+            "notices": request.form.get('notices')
+        }
+
+        event_json = json_util.dumps(event)
+        if not mydb.events.find_one({"title":event['title']}):
+            mydb.events.insert_one(event)
+            return "<p> event added! </p>"
+        else:
+            return jsonify({ "error": "title already exist"}), 400
+
+    def modify_event(self):
+        return 0
+
+    def get_all_event(self):
+        return 0
--- user/routes.py
+++ user/routes.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, session, Response, redirect,render_template, url_for
 from app import app
-from user.models import User
+from user.models import User, Event
+import cv2
+from membercam import FaceRecognition_member
+from tkinter import messagebox
+import json
+import webbrowser
+
+
+global_name = None
+result = 0
 
 
 @app.route('/user/signup', methods=['POST'])
@@ -14,12 +23,153 @@ def signout():
 @app.route('/user/login', methods=['POST'])
 def login():
     return User().login()
-
+'''
 @app.route('/user/update_user', methods=['POST'])
 def update_user():
     return User().update_user()
-
+'''
 @app.route('/user/update_user', methods=['POST'])
 def update_user_route():
     user_obj = User()
     return user_obj.update_user()
+
+@app.route("/admin", methods = ['GET'])
+def get_all_member():
+    return User().get_all_member()
+
+@app.route("/delete_member/<string:email>", methods = ['GET'])
+def delete_member(email):
+    #print("passed routes.py, reaching for User.get_all_member()")
+    return User().delete_member(email)
+
+@app.route("/get_member/<string:email>", methods = ['GET'])
+def get_member(email):
+    return User().get_member(email)
+
+### testing
+@app.route("/test", methods = ['GET'])
+def test_get_all_member():
+    return User().test_get_all_member()
+### end testing
+
+
+@app.route("/add_event", methods = ['POST'])
+def add_event():
+    return Event().add_event()
+
+
+@app.route('/membercam')
+def membercam():
+    user_json = session.get('user')  # Get user JSON from session
+    user_data = json.loads(user_json)  # Parse JSON to dictionary
+    name = user_data['name']
+    print("這裡是setname")
+
+    # 同時也設定全域變數 global_name
+    global global_name
+    global_name = name
+
+    return render_template('membercam.html')
+
+@app.route('/video_feed3')
+def video_feed3():
+    return Response(generate_frames_session(session), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def generate_frames_session(session):
+    fr = FaceRecognition_member()
+    video_capture = cv2.VideoCapture(0)
+
+    correct = 0
+    fail = 0
+
+    if not video_capture.isOpened():
+        return 'Video source not found'
+    count = 0
+
+    while count < 5:
+        ret, frame = video_capture.read()
+        if not ret:
+            break
+
+        frame, recognized_name = fr.run_recognition(frame)
+        recognized_name = recognized_name.split('(', 1)
+
+        # 從 session 中獲取名字
+        global session_name
+        session_name = global_name
+        print(session_name)
+        print(recognized_name)
+        # 比對辨識出來的名字和 session 中的名字是否一致
+        if recognized_name[0] == session_name:
+            print("辨識結果和 session 中的名字一致")
+            #show_success_popup(session_name)
+            correct += 1
+            #return redirect(url_for('recognition_correct'))
+            #flash("辨識結果和 session 中的名字一致", "success")
+        else:
+            print("辨識結果和 session 中的名字不一致")
+            fail += 1
+            #return redirect(url_for('recognition_fail.html'))
+            #flash("辨識結果和 session 中的名字不一致", "error")
+
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+        count += 1
+
+    print(correct, fail)
+    global result
+    result = correct / (correct + fail)
+    video_capture.release()
+
+
+@app.route('/recognition_result')
+def recognition_result():
+    global result
+    if result >= 0.75:
+        print(result)
+        show_success_popup(session_name)
+        return render_template('recognition_correct.html')
+    elif result <0.75:
+        print(result)
+        #show_fail_popup(session_name)
+        return render_template('recognition_fail.html')
+
+@app.route('/recognition_correct')
+def recognition_correct():
+    print('成功進結果網頁')
+    print(result)
+    return render_template('recognition_correct.html')
+
+@app.route('/recognition_fail')
+def recognition_fail():
+    print('成功進判斷式')
+    print(result)
+    return render_template('recognition_fail.html')
+
+
+def show_success_popup(name):
+    message = f"{name} 成功驗證為本人"
+    messagebox.showinfo('Recognition Success', message)  # 訊息內容
+    webbrowser.open('http://127.0.0.1:5000/recognition_correct')
+
+def show_fail_popup(name):
+    message = f"{name} 驗證失敗"
+    messagebox.showwarning(title='Recognition Fail',  # 視窗標題
+                        message=message)  # 訊息內容
+    webbrowser.open('http://127.0.0.1:5000/recognition_fail')
+
+@app.route('/search', methods=['POST'])
+def search():
+    return User().search()
+
+@app.route('/event/<event_id>')
+def event_details(event_id):
+    return User().event_details(event_id)
+
+@app.route("/all_event", methods = ['GET'])
+def all_event():
+    return User.all_event()
+    #return render_template('all_event.html')
